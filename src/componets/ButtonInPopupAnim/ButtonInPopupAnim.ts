07c684169b7ab8cac45dbf96f1d14da763c7336d
mpogodaev <mpogodaev@kldneolant>
2023-05-15 12:15:03 +0200

    button in component

--- src/componets/ButtonInPopupAnim/ButtonInPopupAnim.ts
+++ src/componets/ButtonInPopupAnim/ButtonInPopupAnim.ts
@@ -1,5 +1,13 @@
-const ButtonInPopupAnim = ({ text }: { text: string }) => {
-
+import { createElementNode } from "../../utils/components"
+import styles from './ButtonInPopupAnim.module.scss'
+const ButtonInPopupAnim = ({ text, onClick }: { text: string, onClick: VoidFunction }) => {
+    const $button = createElementNode('div', [styles.buttonActions, 'buttonActions', styles.reverse, styles.dark])
+    $button.innerHTML = '<div><span>' + text.trim().split('').join('</span><span>') + '</span></div>'
+    // button actions
+    // button actions
+    $button.onclick = onClick
+    return $button
 }
+// <div id="pasteViewer" class="buttonActions reverse dark">Копировать/Вставить</div>
 
 export default ButtonInPopupAnim
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -4,9 +4,10 @@ import './popup.scss'
 
 import { gsap } from "gsap";
 import { createElementNode, useState } from "../utils/components";
+import { ButtonInPopupAnim } from '../componets/ButtonInPopupAnim';
 
-type PageNavigatorType = Record<string, () => HTMLElement>
-const selectPage = new useState<string>('1', () => {
+type PageNavigatorType = Record<number, () => HTMLElement>
+const selectPage = new useState<number>(1, () => {
     renderBlock()
 })
 const showModalPasteInterface = () => {
@@ -34,7 +35,7 @@ const showModalPasteInterface = () => {
     });
 }
 function move(id: string, position: string, color: string) {
-    selectPage.update(id)
+    selectPage.update(parseInt(id))
     const tl = gsap.timeline();
 
     tl.to("#bgBubble", { duration: 0.15, bottom: "-30px", ease: "ease-out" }, 0)
@@ -67,6 +68,23 @@ menuElement2.addEventListener('click', () => move('2', '150px', '#81d4fa'))
 menuElement3.addEventListener('click', () => move('3', '250px', '#c5e1a5'))
 menuElemen4.addEventListener('click', () => move('4', '350px', '#ce93d8'))
 
+
+
+const renderOnePage = () => {
+    const wrapper = createElementNode('div')
+    const button = ButtonInPopupAnim({
+        text: 'Копировать/Вставить',
+        onClick: showModalPasteInterface
+    })
+    wrapper.appendChild(button)
+    return wrapper
+}
+const renderTwoPage = () => {
+    const wrapper = createElementNode('div')
+    wrapper.innerHTML = '2'
+    return wrapper
+}
+
 const renderBlock = () => {
     const objPage: PageNavigatorType = {
         '1': renderOnePage,
@@ -80,24 +98,6 @@ const renderBlock = () => {
 }
 renderBlock()
 
-const renderOnePage = () => {
-    const wrapper = createElementNode('div')
-    const warapperButton = createElementNode('div')
-    wrapper.innerHTML = '1'
-    // <div id="pasteViewer" class="buttonActions reverse dark">Копировать/Вставить</div>
-
-    return wrapper
-}
-const renderTwoPage = () => {
-    const wrapper = createElementNode('div')
-    wrapper.innerHTML = '2'
-    return wrapper
-}
-
-
-// button actions
-document.querySelectorAll('.buttonActions').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
-// button actions
 
 
 class RenderWarningTextInPopup {
@@ -118,180 +118,3 @@ class RenderWarningTextInPopup {
         }, 3000)
     }
 }
-// class RenderWarningTextInPopup {
-//     private warningText: string
-//     constructor(message: string) {
-//         this.warningText = message;
-//     }
-//     private body = document.body
-
-//     // wrapperWarnignText
-//     private wrapperWarnignText = createElementNode('div', ['wrapperWarnignText']);
-
-//            wrapperWarnignText.innerText = this.warningText;
-
-// }
-// chrome.runtime.sendMessage('viewers', response => {
-//     console.log(JSON.parse(response), 'response msg');
-// })
-// const tbody = document.querySelector('.tbody')
-// const tbodyStorage = document.querySelector('.tbodyStorage')
-// const isSortViewer = document.getElementById('sortIsCheck') as HTMLInputElement
-// const buttonPasteJS = document.getElementById('button')
-// console.log("🚀 ~ file: popup.ts:11 ~ buttonPasteJS:", buttonPasteJS)
-// setInterval(() => {
-//     (async () => {
-//         const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-//         const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-//         // do something with response here, not outside the function
-//         console.log(response);
-//       })();
-// }, 1000)
-// buttonPasteJS.addEventListener('click', () => {
-//     console.log(1231);
-//     // console.log("🚀 ~ file: popup.ts:24 ~ chrome.tabs.onActivated.addListener ~ tab:", tab)
-// })
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     const viewers = request.message.payload as Viewer[]
-//     if (request.action !== "viewers") return
-//     console.log("🚀 ~ file: popup.ts:11 ~ request:", request)
-//     console.log("🚀 ~ file: popup.ts:11 ~ viewers:", viewers)
-//     const renderViewers = viewers.filter(item => item.Name)
-//     isSortViewer.addEventListener('click', () => {
-//         console.dir(isSortViewer?.checked)
-//         for (const item of viewers) {
-//             const row = document.createElement('tr')
-//             const cellName = document.createElement('td')
-//             cellName.innerText = item.Caption
-//             const cellPlus = document.createElement('td')
-//             cellPlus.innerText = '+'
-//             cellPlus.setAttribute('id', item.Id)
-//             cellPlus.onclick = (e: any) => {
-//                 // console.log(e.target.getAttribute('id'));
-//                 const id = e.target.getAttribute('id')
-//                 addIdViewersStorage(id, viewers)
-//             }
-//             row.append(cellName)
-//             row.append(cellPlus)
-//             tbody.append(row)
-//         }
-//     })
-// });
-// const addNestedTable = (viewers: any, resetTable?: boolean) => {
-//     if (resetTable) tbodyStorage.innerHTML = ''
-
-
-//     for (const currentView of viewers) {
-//         const row = document.createElement('tr')
-//         const cellName = document.createElement('td')
-//         cellName.innerText = currentView.Caption
-//         const cellMinus = document.createElement('td')
-//         cellMinus.innerText = '-'
-//         cellMinus.onclick = () => {
-//             deleteView(currentView.Id)
-//         }
-//         const cellPaste = document.createElement('td')
-//         cellPaste.onclick = () => {
-
-//             // insertInEntities(currentView)
-//         }
-//         cellPaste.innerText = 'Вставить'
-//         row.append(cellName)
-//         row.append(cellMinus)
-//         row.append(cellPaste)
-//         tbodyStorage.append(row)
-//     }
-// }
-// chrome.storage.onChanged.addListener((changes, namespace) => {
-//     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-//         const viewers = newValue && JSON.parse(newValue)
-//         addNestedTable(viewers, true)
-//     }
-// });
-// const addIdViewersStorage = async (id: any, viewers: any[]) => {
-//     const currentView = viewers?.find((item: { Id: any; }) => item.Id === id)
-//     chrome.storage.local.get(["viewersState"], function (result) {
-//         const allView = result.viewersState && JSON.parse(result.viewersState)
-//         const saveViewersStorage = Array.isArray(allView) ? allView : []
-//         saveViewersStorage.push(currentView)
-//         console.log("🚀 ~ saveViewersStorage:", saveViewersStorage)
-
-//         chrome.storage.local.set({
-//             viewersState: JSON.stringify(saveViewersStorage)
-//         }, function () {
-//             console.log("Данные сохранены");
-//         });
-//     });
-// }
-// const renderSaveViewers = async () => {
-//     chrome.storage.local.get(["viewersState"], function (result) {
-//         console.log("🚀 ~ result:", result.viewersState)
-//         const viewers = result.viewersState && JSON.parse(result.viewersState)
-//         viewers && addNestedTable(viewers)
-//     });
-// }
-// renderSaveViewers()
-
-// const insertInEntities = async (currentView: { Caption: any; Icon: any; Attributes: any; Settings: any; }) => {
-
-//     chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
-//         const idEntites = getParamFromUrl(tabs[0].url).id
-//         try {
-//             const responseCreate = await fetch(`https://lukoil-test.io.neolant.su/api/structure/entities/${idEntites}/viewers`, {
-//                 method: "POST",
-//                 body: JSON.stringify({
-//                     "Name": "VIEWER_EXTERNAL",
-//                     "Caption": currentView.Caption,
-//                     "Icon": currentView.Icon,
-//                     "Attributes": currentView.Attributes,
-//                     "Settings": currentView.Settings
-//                 }),
-//                 headers: {
-//                     "Accept": "application/json, text/plain, /",
-//                     "Accept-Encoding": "gzip, deflate, br",
-//                     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-//                     "Connection": "keep-alive",
-//                     "Content-Length": "103",
-//                     "Content-Type": "application/json;charset=UTF-8"
-//                 }
-//             }).then(_ => _.json())
-//             console.log(responseCreate)
-
-//         } catch (error) {
-//             console.log("🚀 ~ error:", error)
-//         }
-//     })
-
-// }
-
-// const deleteView = (id: string) => {
-//     chrome.storage.local.get(["viewersState"], function (result) {
-//         const allView = result.viewersState && JSON.parse(result.viewersState)
-//         const saveViewersStorage = allView.filter((item: any) => item.Id !== id)
-
-//         chrome.storage.local.set({
-//             viewersState: JSON.stringify(saveViewersStorage)
-//         }, function () {
-//             console.log("Данные сохранены");
-//         });
-//     });
-// }
-
-// const getParamFromUrl = (url: string) => {
-//     // Разбиваем строку запроса на отдельные параметры
-//     const params = new URLSearchParams(url);
-
-//     // Создаем пустой объект для хранения параметров
-//     const paramsObj: Record<string, string> = {};
-
-//     // Проходимся по всем параметрам и добавляем их в объект
-//     params.forEach((value, key) => {
-//         const keyArr = key?.split("?")
-
-//         const keyValue = keyArr.length > 1 ? keyArr[1] : keyArr[0]
-
-//         paramsObj[keyValue] = value;
-//     });
-
-//     return paramsObj
-// }
